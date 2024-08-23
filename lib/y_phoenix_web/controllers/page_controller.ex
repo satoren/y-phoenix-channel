e2d905463cff7ec75761c2df12737ea0d832450c
satoren <satoreyo@hotmail.com>
2024-08-23 22:36:04 +0900

    add block note example

--- lib/y_phoenix_web/controllers/page_controller.ex
+++ lib/y_phoenix_web/controllers/page_controller.ex
@@ -2,8 +2,13 @@ defmodule YPhoenixWeb.PageController do
   use YPhoenixWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :quill, layout: false)
   end
+  def quill(conn, _params) do
+    render(conn, :quill, layout: false)
+  end
+  def blocknote(conn, _params) do
+    render(conn, :blocknote, layout: false)
+  end
+
 end
